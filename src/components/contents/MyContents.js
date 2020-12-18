b289aaa22689069ba66ba2d305d200d58ed596a2
saint-prince <luckyericliu@gmail.com>
2020-12-18 14:44:47 +0100

    Styling MyContents Page

--- src/components/contents/MyContents.js
+++ src/components/contents/MyContents.js
@@ -1,10 +1,15 @@
 import React from "react"
+import "./contents.css"
+// import * as BiIcons from "react-icons/bi"
+import "../Auth/auth.css"
 
 function MyContents () {
     return (
-        <>
-            <h1> Create new Content </h1>
-            <input type="search" placeholder="Search for Content" />
+        <>    
+            {/* <label htmlFor="find"><BiIcons.BiSearchAlt2 /> </label> */}
+            <input id="find" className="search" type="search" placeholder="Search for a content" />  <br/> <br/>
+            <p align="right"><button className="btn btn-lg btn-success"> Create Content </button> </p>
+            
             <h2>All my Contents</h2>
         </>
     ) 
