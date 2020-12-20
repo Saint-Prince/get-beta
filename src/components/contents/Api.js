eb32ee681dc73b2be0d9411dbb8736a33af341c0
saint-prince <luckyericliu@gmail.com>
2020-12-20 03:43:31 +0100

    started with api to get all contents under a user

--- src/components/contents/Api.js
+++ src/components/contents/Api.js
@@ -1,2 +1,10 @@
 import Axios from "axios"
 
+export const getContents = (token) => 
+    Axios.get (       
+        `http://localhost:5000/apiv1/vendors/contents/myContents`,
+        {
+            headers: { "x-auth-token": token }
+        }
+    ).then(data => console.log(data.data.myContents))
+        .catch(err => console.log(err))
--- src/components/contents/MyContents.js
+++ src/components/contents/MyContents.js
@@ -1,16 +1,41 @@
-import React from "react"
+import React, { useContext, useEffect, useState } from "react"
+import UserContext from "../context/UserContext"
+import { useHistory} from "react-router-dom"
+import { getContents } from "./Api"
 import "./contents.css"
-// import * as BiIcons from "react-icons/bi"
+// import * as BiIcons from "react-icons/bi", Link 
 import "../Auth/auth.css"
 
 function MyContents () {
+
+    const { userData } = useContext(UserContext);
+    const history = useHistory();
+    const [contents, setContents] = useState([])
+
+    useEffect(() => {
+        
+        if (! userData.user) 
+            return history.push("/login")
+
+        const fetchContents = async () => {
+            let token = userData.token
+            const details = await getContents(token)
+            setContents(details)
+            console.log(contents)
+        }
+        fetchContents();
+    }, [history, userData.user, userData.token])
+
     return (
         <>    
-            {/* <label htmlFor="find"><BiIcons.BiSearchAlt2 /> </label> */}
             <input id="find" className="search" type="search" placeholder="Search for a content" />  <br/> <br/>
+            {/* <label htmlFor="sort"> Sort By: </label>
+            <select>
+                <option value="recent"> Recently Added </option>
+                <option value="recent"> Recently </option>
+            </select> */}
             <p align="right"><button className="btn btn-lg btn-success"> Create Content </button> </p>
             
-            <h2>All my Contents</h2>
         </>
     ) 
 }
